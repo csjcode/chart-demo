abcd482c7066d26dbe511d06da4419261ea0947e
Chris St. John <csjcode@gmail.com>
2024-02-29 13:45:19 -0700

    Refactor table component to improve readability and styling

--- components/Tables/MyTable.tsx
+++ components/Tables/MyTable.tsx
@@ -10,46 +10,39 @@ import {
   useReactTable,
 } from "@tanstack/react-table";
 import { makeData, Person } from "./makeData";
-
 const columns: ColumnDef<Person>[] = [
   {
-    header: "Transactions",
+    accessorKey: "firstName",
+    cell: (info) => info.getValue(),
     footer: (props) => props.column.id,
-    columns: [
-      {
-        accessorKey: "firstName",
-        cell: (info) => info.getValue(),
-        footer: (props) => props.column.id,
-        header: () => <span>Type</span>,
-      },
-      {
-        accessorFn: (row) => row.lastName,
-        id: "lastName",
-        cell: (info) => info.getValue(),
-        header: () => <span>USD</span>,
-        footer: (props) => props.column.id,
-      },
-      {
-        accessorKey: "age",
-        header: () => "WEN",
-        footer: (props) => props.column.id,
-      },
-      {
-        accessorKey: "visits",
-        header: () => <span>SOL</span>,
-        footer: (props) => props.column.id,
-      },
-      {
-        accessorKey: "status",
-        header: "Price",
-        footer: (props) => props.column.id,
-      },
-      {
-        accessorKey: "progress",
-        header: "Maker",
-        footer: (props) => props.column.id,
-      },
-    ],
+    header: () => <span>Type</span>,
+  },
+  {
+    accessorFn: (row) => row.lastName,
+    id: "lastName",
+    cell: (info) => info.getValue(),
+    header: () => <span>USD</span>,
+    footer: (props) => props.column.id,
+  },
+  {
+    accessorKey: "age",
+    header: () => "WEN",
+    footer: (props) => props.column.id,
+  },
+  {
+    accessorKey: "visits",
+    header: () => <span>SOL</span>,
+    footer: (props) => props.column.id,
+  },
+  {
+    accessorKey: "status",
+    header: "Price",
+    footer: (props) => props.column.id,
+  },
+  {
+    accessorKey: "progress",
+    header: "Maker",
+    footer: (props) => props.column.id,
   },
 ];
 
@@ -66,59 +59,55 @@ export function MyTable() {
     debugHeaders: true,
     debugColumns: true,
   });
-
+  const getRowClassName = (row: any) => {
+    const status = row.original.status; // Adjust based on how your data defines price movement
+    if (status === "buy") return "text-green";
+    if (status === "sell") return "text-red";
+    return "text-silver"; // Assuming 'unchanged' or any other status results in silver
+  };
   return (
     <div className="p-2 block max-w-full overflow-x-scroll overflow-y-hidden">
       <div className="h-2" />
-      <table className="w-full ">
+      <table className="w-full">
         <thead>
           {table.getHeaderGroups().map((headerGroup) => (
             <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <th
-                    key={header.id}
-                    colSpan={header.colSpan}
-                    style={{ position: "relative", width: header.getSize() }}
-                  >
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                    {header.column.getCanResize() && (
-                      <div
-                        onMouseDown={header.getResizeHandler()}
-                        onTouchStart={header.getResizeHandler()}
-                        className={`resizer ${
-                          header.column.getIsResizing() ? "isResizing" : ""
-                        }`}
-                      ></div>
-                    )}
-                  </th>
-                );
-              })}
+              {headerGroup.headers.map((header) => (
+                <th
+                  key={header.id}
+                  colSpan={header.colSpan}
+                  style={{ position: "relative", width: header.getSize() }}
+                >
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                  {header.column.getCanResize() && (
+                    <div
+                      onMouseDown={header.getResizeHandler()}
+                      onTouchStart={header.getResizeHandler()}
+                      className={`resizer ${
+                        header.column.getIsResizing() ? "isResizing" : ""
+                      }`}
+                    ></div>
+                  )}
+                </th>
+              ))}
             </tr>
           ))}
         </thead>
         <tbody>
-          {table.getRowModel().rows.map((row) => {
-            return (
-              <tr key={row.id}>
-                {row.getVisibleCells().map((cell) => {
-                  return (
-                    <td key={cell.id} style={{ width: cell.column.getSize() }}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </td>
-                  );
-                })}
-              </tr>
-            );
-          })}
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id} className={getRowClassName(row)}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id} style={{ width: cell.column.getSize() }}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
         </tbody>
       </table>
       <div className="h-4" />
